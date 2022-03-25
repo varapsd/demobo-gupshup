1de689ca3c4948d66fbccfaaec0f9fc9497e07a5
varapsd <varaprasad3638@gmail.com>
2022-03-25 18:27:32 +0530

    flow

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const cors = require('cors');
 require('dotenv').config();
 
 const options = {cors: {
