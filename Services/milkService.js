2ad297f6581a54e0101303861569fcca5b33181d
varapsd <varaprasad3638@gmail.com>
2022-04-18 12:16:22 +0530

    flow

--- Services/milkService.js
+++ Services/milkService.js
@@ -188,7 +188,7 @@ const productDetails = async(req) =>{
         "content":{ 
             "type":"text", 
             "header": productDetailsDTO.name, 
-            "text":`${productDetailsDTO.description}\nAvailable In : ${productDetailsDTO.available}\nMRP : ${productDetailsDTO.mrp}\nBottle Shelf Life : ${productDetailsDTO.BottleShellLife ? productDetailsDTO.BottleShellLife : "NONE"}`
+            "text":`\n${productDetailsDTO.description}\nAvailable In : ${productDetailsDTO.available}\n\nMRP : ${productDetailsDTO.mrp}\n\nBottle Shelf Life : ${productDetailsDTO.BottleShellLife ? productDetailsDTO.BottleShellLife : "NONE"}`
         },
         "options":[ 
             { 
